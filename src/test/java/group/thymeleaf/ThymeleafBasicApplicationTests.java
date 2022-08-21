16676eab38262b321c5c3cafb38d824e6cd9aa28
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2022-08-21 17:42:06 +0900

    Initial commit

---
+++ src/test/java/group/thymeleaf/ThymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package group.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
